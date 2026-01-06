90a8379e22320e9928645ec8d62b34ac69dd63ef
zach <97859794+zvchvry@users.noreply.github.com>
2026-01-05 23:28:34 -0500

    style update

--- dead-works/app/page.tsx
+++ dead-works/app/page.tsx
@@ -4,7 +4,7 @@ import { Typewriter } from "@/components/Typewriter";
 export default function Page() {
   return (
     <main className="mx-auto max-w-5xl p-6">
-      <h1 className="mb-4 text-2xl font-bold">[ dead.works ]</h1>
+      <h1 className="mb-4 font-bold italic">[ dead.works ]</h1>
 
       <Typewriter
         text={">a list of 𝕲𝖍𝖔𝖚𝖑𝖘 projects and derivatives\n>can't kill what's already dead"}
